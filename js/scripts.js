ce801658a35721b01cf144440ad28ad30896e251
weslwood <weslwood@hotmail.com>
2021-10-14 20:28:01 -0400

    Test 2

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+$(function() {
+    $(".carousel").carousel( { interval: 2000 } );
+    $("#carouselButton").click(function(){
+        if ($("#carouselButton").children("i").hasClass("fa-pause")) {
+            $(".carousel").carousel("pause");
+            $("#carouselButton").children("i").removeClass("fa-pause");
+            $("#carouselButton").children("i").addClass("fa-play");
+        } else {
+            $(".carousel").carousel("cycle");
+            $("#carouselButton").children("i").removeClass("fa-play");
+            $("#carouselButton").children("i").addClass("fa-pause");
+        }
+    });
+});
+
+// needs debug
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip();
+});
